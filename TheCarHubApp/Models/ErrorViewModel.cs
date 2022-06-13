93f9c4f1b4794d87f5ed7992a99a5c61f1f4f268
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2022-06-13 19:45:18 +0200

    Add project files.

---
+++ TheCarHubApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheCarHubApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
